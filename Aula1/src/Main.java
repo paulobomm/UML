2b4b8628019f7fac8cf41fbfd46d4c9b11ba9e9f
paulobomm <paulobomm@gmail.com>
2024-07-30 21:58:09 -0300

    Aula1

--- Aula1/src/Main.java
+++ Aula1/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
+    //public static void main(String[] args) {
+        //String nome = "Paulo";
+        //System.out.println("Helo ".toUpperCase() + nome.toUpperCase());
+        //}
+    //
+//
+//}
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Operacao operacao = new Operacao();
+        operacao.soma(2.0, 2.5);
+        operacao.sub(2.00 , 2.5);
+        operacao.mult(2.00 , 2.5);
+        operacao.divi(2.00 , 2.5);
     }
-}
+
+
+  }
---
+++ Aula1/src/Operacao.java
@@ -0,0 +1,23 @@
+public class Operacao {
+
+    public void soma(Double x, Double y){
+        System.out.println("Soma de " + x + " + " + y + " = " + (x+y));
+
+    }
+    public void sub(Double x, Double y){
+        System.out.println("Subtracao de " + x + " - " + y + " = " + (x-y));
+
+    }
+    public void mult(Double x, Double y){
+        System.out.println("Multiplicacao de " + x + " x " + y + " = " + (x*y));
+
+    }
+    public void divi(Double x, Double y){
+        System.out.println("Divisao de " + x + " / " + y + " = " + (x/y));
+
+    }
+
+}
+
+
+
