3413556ca3de52800c39bbdeeb4bcbdf97fcdcca
paulobomm <paulobomm@gmail.com>
2024-08-27 20:25:31 -0300

    teste01

--- Aula 03/Exercicios/ListaExercicio03/src/Exercicio01/Livro.java
+++ Aula 03/Exercicios/ListaExercicio03/src/Exercicio01/Livro.java
@@ -1,2 +1,16 @@
-package Exercicio01;public class Livro {
+package Exercicio01;
+/* 1 - Crie uma classe chamada Livro com atributos
+ como titulo, autor, e numeroDePaginas. Adicione métodos para
+ abrir o livro e ler uma página.*/
+
+public class Livro {
+    public String titulo;
+    public String autor;
+    public Integer numeroDePaginas;
+    private Boolean livroAberto = false;
+    private Integer paginaAtual = 1;
+    private Boolean concluido = false;
+
+
+
 }
